9e0e8f2741fa5fe4c4642f6131626989ce31755b
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-01 16:16:26 -0300

    Update conta.cpp

--- Aula5/ExemploClasse/Exemplo/conta.cpp
+++ Aula5/ExemploClasse/Exemplo/conta.cpp
@@ -6,6 +6,7 @@ Conta::Conta() //Construtor da Classe conta, quando nenhum parametro é declarad
     this->numero = 0;
     this->senha = 1111;
     this->titular = "Nenhum";
+    this->tipo = "Corrente";
     this->saldo = 0;
     std::cout << "Construtor padrao invocado"<<std::endl;
 }
@@ -48,7 +49,7 @@ double Conta::getSaldo(int senha) //Metodo que mediante a inserção de uma senh
     else
     {
         std::cout<<"Senha inválida"<<std::endl;
-        return -1000000;
+        return -1000000; //nao e uma boa pratica
     }    
 
 }
@@ -100,3 +101,41 @@ void Conta::saque(int senha, double valor)// Metodo que retira um valor do atrib
     }
     
 }
+
+/*
+Problemas:
+1) Construtor default faz uma interface com o usuario. Responsabilidade dividida, interface com o 
+usuario e fazendo o papel de manipular a conta do usuario
+
+Guilherme: Interface com o usuario nos metados da Classe
+
+2) Repeticao de codigo, validacao do saldo e feita em varias partes do codigo. Saldo deveria ser 
+verificado apenas na funcao setSaldo
+
+3) Funcao publica para modificar a senha sem nenhum tipo de validacao (senha anterior ou canal 
+    externo, email e etc)
+    
+4) Usuario recebendo -1000000. 
+Guilherme: funcoes sem retorno de status
+Correcao: funcoes com retorno de status (flags)
+ex: #define OP_SUCCESS 0
+    #define ERRO_SALDO -1
+    
+    passagem por referencia, passa os valores a serem retornados como parametros da funcao:
+    int Conta:: getSaldo(int senha, double saldo)
+    {
+        if(senha == this->senha)
+        {
+            saldo = this->saldo;
+            return OP_SUCCESS;
+        }
+        else
+        {
+            return ERRO_SALDO;
+        }
+    }
+
+*/
+
+
+
