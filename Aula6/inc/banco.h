cf5e8b74e45d20a9f7c3e418a5010b80e0029c17
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-02 15:11:22 -0300

    Create banco.h

---
+++ Aula6/inc/banco.h
@@ -0,0 +1,20 @@
+#ifndef BANCO_H
+#define BANCO_H
+
+#include "conta.h"
+
+#define NUMCONTAS 100 //Define que a palavar NUMCONTAS passa a valer como escrever 100
+
+class Banco
+{
+private:
+    Conta contas[NUMCONTAS];//Cria um vetor de objetos do tipo Conta que pode armazenar até 100 contas
+public:
+    Banco();
+    ~Banco();
+    Conta* buscaConta(int numero); //Metodo que retorna o endereço do objeto conta que possui o mesmo numero informado
+    void atendimento();
+};
+
+
+#endif
