811b8517e81572e39d2308b0bfa3f35f6c580064
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-08 15:18:48 -0300

    Update main.cpp

--- Aula7/ClasseEmpacotador/main.cpp
+++ Aula7/ClasseEmpacotador/main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include "empacotador.h"
+using namespace std;
 
+
+void foo() //Chama a função imprimeTotalEmpacotados(), ela não precisa de um objeto
+{
+    Empacotador::imprimeTotalEmpacotados();
+}
+
+int main()
+{
+    foo();
+    
+    Empacotador emp1(1), emp2(2);
+
+    emp1.empacotar(10);
+    emp2.empacotar(20);
+    emp1.empacotar(30);
+
+    emp1.imprimeNumEmpacotados();
+    emp2.imprimeNumEmpacotados();
+
+    Empacotador::imprimeTotalEmpacotados();
+    Empacotador::imprimeTotalEmpacotados();
+    Empacotador::imprimeTotalEmpacotados();
+    Empacotador::imprimeTotalEmpacotados();
+
+    foo();
+
+    return 0;
+}
