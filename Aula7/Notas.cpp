a3e611c16031db369efae44b7ff7e7d1399c5bb1
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-08 16:27:30 -0300

    Update Notas.cpp

--- Aula7/Notas.cpp
+++ Aula7/Notas.cpp
@@ -13,18 +13,39 @@ int main()
 // Modificadores mudam a forma na qual as variaveis sao trabalhadas
 const // torna a variavel para apenas leitura
 static 
-  // com variaveis locais:
-  void foo()
+  // com variaveis locais: variavel local nao e mais desalocada no final de execucao de sua funcao
+    // variavel estatica continua com o seu escopo na funcao local, ela nao e vista na main, diferente das variaveis globais
+    void foo()
+    {
+      static int cont = 0; // cont sera armazenado na regiao de memoria "data" ao inves da "stack", ou seja, cont nao sera desalocada mais
+      cout << ++cont;
+    }
+  
+    int main()
+    {
+      foo(); // imprime 1
+      foo(); // imprime 2
+      foo(); // imprime 3
+    }
+// com objetos:
+  class C
   {
-    int cont = 0;
-    cout << ++cont;
+    private:
+      static int attr; // como attr vai para o data, o attr1 sera compartilhado entre c1 e c2, logo o programa gasta 12 bytes, mas attr2 sera individual
+      int attr2;
   }
-
+  static void set2()
+  {
+    this -> attr2 = 10; // metodo estatico nao tem acesso ao ponteiro "this", pois nao esta vinculado a um objeto em especifico. PORTANTO: metodo estatico so manipula 
+                         // atributos estaticos   
+  }
   int main()
   {
-    foo();
-    foo();
-    foo();
+    C c1;
+    C c2;
+    C::attr1 = 10; // this->attr1 = 10; nao funciona, pois attr1 nao e de variavel, mas sim da classe    
   }
+// 
 
+  
 
