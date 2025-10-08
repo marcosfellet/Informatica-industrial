0c292fab044072c890a6912a57533a27ae3aa78e
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-08 15:17:57 -0300

    Update empacotador.h

--- Aula7/ClasseEmpacotador/empacotador.h
+++ Aula7/ClasseEmpacotador/empacotador.h
@@ -1,1 +1,20 @@
+#ifndef EMPACOTADOR_H
+#define EMPACOTADOR_H
 
+class Empacotador
+{
+private:
+    int numEmpacotados;
+    int id;
+    static int totalEmpacotados;
+public:
+    Empacotador(int);//Construtor da classe Empacotador
+    void empacotar(int);
+    int getNumEmpacotados();
+    static int getTotalEmpacotados();
+    void imprimeNumEmpacotados();
+    static void imprimeTotalEmpacotados();
+};
+
+
+#endif
