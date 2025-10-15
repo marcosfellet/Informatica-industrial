f00fffe1b1ba6265c6e5101f7cb9d0d62a4a5ab2
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-15 16:34:32 -0300

    Update Notas.cpp

--- Aula9/Notas.cpp
+++ Aula9/Notas.cpp
@@ -2,7 +2,7 @@
   acrescenta atributos, metodos e modificar metodos existentes
 */
 
-/***************************************************************EXEMPLO: JOGO DE LUTA***************************************************************/
+/***************************************************************EXEMPLO: JOGO DE LUTA (HERANCA)***************************************************************/
 Class Personagem
 {
   private: 
@@ -24,4 +24,28 @@ Class Personagem
     atacar();
 
 // protected: tipo de atributo que permite que apenas a classe e a suas derivadas "enchergam" os atributos
+
+/***************************************************************POLIMORFISMO***************************************************************/
+/*
+        no C++:
+            o polimorfismo nao e automatico
+            utilizar a palavra "virtual" antes da funcao da funcao da classe base
+            Polimorfismo funciona apenas, se ponteiros de objetos estiverem sendo utilizados!!
+        Polimorfismo esta relacionado a metodos e NAO A CLASSES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        funcao de forma pura: funcao a ser sobrescrita por uma funcao de uma classe derivada
+*/
+           virtual void atacar() = 0;
+
+
+
+
+
+
+
+
+
+
+
+
 
