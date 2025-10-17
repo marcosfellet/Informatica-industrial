becceb11528dae4d472806c15644dec69b3acf3b
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-17 11:30:17 -0300

    Update MobileRobot.h

--- Aula9/Desafios/MobileRobot.h
+++ Aula9/Desafios/MobileRobot.h
@@ -1,1 +1,13 @@
 
+class Robot
+{
+  private:
+    double PosicaoAtual[3];
+
+  public:
+    double getPosicaoAtual(char coordenada);
+    void setPosicaoAtual(double X, double Y, double Z);
+    virtual void Mover(double Xvel, double Yvel, double Zvel, double tempo) =0;
+
+
+}
