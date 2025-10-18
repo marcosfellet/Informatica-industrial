0e53b4506aedd9b83e65a931f4b8abe0e903b08e
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-18 17:12:40 -0300

    Update mobilerobot.cpp

--- Aula9/Desafios/mobilerobot.cpp
+++ Aula9/Desafios/mobilerobot.cpp
@@ -1,19 +1,67 @@
 #include "mobilerobot.h"
-#include <"iostream.h">
+#include <iostream>
 
-double getPosicaoAtual(char coordenadas)
+using namespace std;
+
+Robot::Robot(char senha, double x, double y, double z) // construtor dos objetos robos
+{
+    this->senha = senha;
+    x = PosicaoAtual[0];
+    y = PosicaoAtual[1];
+    z = PosicaoAtual[2];
+}
+~Robot::Robot() // destrutor dos objetos robos
 {
     
-  
 }
-void setPosicaoAtual(double x, double y, double z)
+
+int Robot::validaSenha(int senha)
+{
+    if(senha == this->senha)
+    {
+        return SENHA_CORRETA;
+    }
+    else
+    {
+        return ERROR;
+    }
+}
+
+double Robot::getPosicaoAtual(char coordenada)
+{
+    double posicao;
+    if(validaSenha() == SENHA_CORRETA)
+    {
+        if(coordenada == 'x')
+        {
+            posicao = this->x;
+            cout<<"Posicao em x: "<< posicao <<endl;
+        }
+        else if(coordenada == 'y')
+        {
+            posicao = this->y;
+            cout<<"Posicao em z: "<< posicao <<endl;
+        }
+        else if(coordenada == 'z')
+        {
+            posicao = this->z;
+            cout<<"Posicao em z: "<< posicao <<endl;
+        }
+        else
+        {
+            cout<<"Coordenada não suportada ou invalida"<<endl;    
+        }
+    }else
+    {
+        cout<<"Senha Incorreta"<<endl;
+    }
+   
+      
+}
+void Robot::setPosicaoAtual(double x, double y, double z)
 {
   
   
 }
-virtual void mover(double xvel, double yvel, double zvel, double tempo)
-{
-  
-  
-}
+
 
