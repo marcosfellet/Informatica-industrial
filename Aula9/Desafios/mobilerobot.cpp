7224c7942db25ac13b11e7b80a4e812d816f3108
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-17 21:52:49 -0300

    Update mobilerobot.cpp

--- Aula9/Desafios/mobilerobot.cpp
+++ Aula9/Desafios/mobilerobot.cpp
@@ -1,2 +1,19 @@
 #include "mobilerobot.h"
 #include <"iostream.h">
+
+double getPosicaoAtual(char coordenadas)
+{
+    
+  
+}
+void setPosicaoAtual(double x, double y, double z)
+{
+  
+  
+}
+virtual void mover(double xvel, double yvel, double zvel, double tempo)
+{
+  
+  
+}
+
