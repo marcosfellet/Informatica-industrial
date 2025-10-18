e1c300887829f02444200a4838bd8c8bcbe65764
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-18 15:46:41 -0300

    Update mobilerobot.h

--- Aula9/Desafios/mobilerobot.h
+++ Aula9/Desafios/mobilerobot.h
@@ -9,8 +9,10 @@ class Robot
 {
   private:
     double PosicaoAtual[3];
-
+    double x, y, z;
   public:
+    Robot(x, y, z);
+    ~Robot();
     double getPosicaoAtual(char coordenada);
     void setPosicaoAtual(double X, double Y, double Z);
     virtual void Mover(double Xvel, double Yvel, double Zvel, double tempo) =0;
@@ -24,7 +26,7 @@ class Quadrotor: public Robot
   private:
 
   public:
-
+  Mover(double Xvel, double Yvel, double Zvel, double tempo);
   
 
 class RoboTerrestre: public Robot
@@ -32,7 +34,7 @@ class RoboTerrestre: public Robot
   private:
     
   public:
-
+  Mover(double Xvel, double Yvel, double Zvel, double tempo);
 
 
 }
