e5bf2596a38f5fa806e31e61c5ddfa23c854112b
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-23 15:53:55 -0300

    Update Notas_Python.cpp

--- Aula1/Notas_Python.cpp
+++ Aula1/Notas_Python.cpp
@@ -1,1 +1,6 @@
-
+/*
+Versao x.y.z
+x => versao master, se trocada, nao roda em versoes antigas
+y => adicao de novas funcionalidades
+z => correcao de bugs e etc, nao apresenta incompatibilidade 
+*/
