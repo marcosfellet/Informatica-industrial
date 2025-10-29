ebbced85283dc96ff9d3b4d8a9d165fa3e757076
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-29 15:26:55 -0300

    Update notas.py

--- Aula11/notas.py
+++ Aula11/notas.py
@@ -1,4 +1,4 @@
-#################################### COLECOES NAO-ORDENADAS#######################################################
+#################################### COLECOES NAO-ORDENADAS #######################################################
 # listas = armazena elementos, acesso por meio de indices mutavel
   l = [1,10,150,"ENE118  "]
   l.append(30)
@@ -6,14 +6,26 @@ print(l[1])
 # tuplas = armazena elementos, acesso por meio de indices imutavel
   t = (1,2,5,6)
   print(t[2])
-# dicionarios = armazena elementos, acesso por meio de chaves unicas, carrega a semantica do dado junto.
+# dicionarios = armazena elementos, acesso por meio de chaves unicas, carrega a semantica do dado junto (o que o dado significa)
   d = {"curso": "Engenhaia", "Ch": 3600}
 #        key       value
   print(d["curso"])
 
-#################################### COLECOES ORDENADAS#######################################################
+#################################### COLECOES ORDENADAS #######################################################
 # pilhas
+# filas
 
+#################################### DESAFIO #######################################################
+#Dica:
+catalogo = {
+  "p000" = {"Descricao": ..., "Faixa de Valores, ..., ..." },
+  "p001" = {},
+  "p002" = {},
+  .
+  .
+  .
+}
 
-
+print(catalogo["p000"]["Descricao"])
+#  p000 = chave principal
 
