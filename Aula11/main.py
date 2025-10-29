87a1927628c67492eb96d5707b3d3e1e1d64c3fd
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-10-29 15:19:00 -0300

    Create main.py

---
+++ Aula11/main.py
@@ -0,0 +1,19 @@
+#################################### COLECOES NAO-ORDENADAS#######################################################
+# listas = armazena elementos, acesso por meio de indices mutavel
+  l = [1,10,150,"ENE118  "]
+  l.append(30)
+print(l[1])
+# tuplas = armazena elementos, acesso por meio de indices imutavel
+  t = (1,2,5,6)
+  print(t[2])
+# dicionarios = armazena elementos, acesso por meio de chaves unicas, carrega a semantica do dado junto.
+  d = {"curso": "Engenhaia", "Ch": 3600}
+#        key       value
+  print(d["curso"])
+
+#################################### COLECOES ORDENADAS#######################################################
+# pilhas
+
+
+
+
