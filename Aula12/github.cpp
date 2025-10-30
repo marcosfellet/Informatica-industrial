89748390ccb3c6ea6ad1423371259146e4b9e716
butturinipedro-eng <butturini.pedro@estudante.ufjf.br>
2025-10-30 16:35:16 -0300

    Update github.cpp

--- Aula12/github.cpp
+++ Aula12/github.cpp
@@ -1,10 +1,17 @@
 /*
-
-
-
-
-
-
-
-
+#git status
+No commits yet arquivo não esta sendo rastreado
+git add . adiciona todos os arquivos ao git
+git commit -m "feat: Mensagem referente a versão atual", salva o progresso
+git log --oneline Resumo de cada commit em uma linha
+git diff mudanças que ainda nao foram commitadas
+git diff <commit1><commit2> compara dois commits especificos
+git restore descarta as alterações não commitadas
+git reset muda e limpa um commit feito
+git revert cria um novo commit com as alterações, mantém o anterior salvo e não apaga o histórico
+branch é uma ramificação do projeto
+branch main utilizada soemnte com códigos testados e validados
+quando for fazer alterações clonar a main em outro branch e trabalhar neste branch específico
+TDD test driven development testes automáticos de features caso haja novas alterações
+git pull baixa e integra as mudanças
 */
