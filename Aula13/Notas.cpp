6226208cbf3c63824e53fb29d0282b68557ef924
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-11-05 16:34:06 -0300

    Update Notas.cpp

--- Aula13/Notas.cpp
+++ Aula13/Notas.cpp
@@ -25,7 +25,51 @@
                          mas, o cliente precisa saber o endereço do google para usá-lo
       - Padrão Publisher-Subscriber
           - Nós de recebimento e nós de emissão de dados
-      
+      - API Socket = Interface de Programação de Aplicativos
+          - Interface provida pelo sistema operaciona, que permite a interface acessar os periféricos de rede
+          - *********** Ver fluxograma de como funciona API Socket 
+          - Aplicação de comunicação 
+              - Execução do servidor
+              - Criar socket no servidor
+                1) Objeto do código
+                2) Família de entereços para qual vai atender requisições
+                    - IPv4, IPv6
+                3) Comunicação TCP ou UDP
+                4) Primitiva bind (anexar)*
+                    - Pedir pro sistema operacional para fornecer um serviço num IP numa placa
+                    - Como se tivesse assinando o contrato de aluguel de uma porta para uma dada funcionalidade (não pode ter dois aplicativos numa mesma porta)
+                    * Geração de excessão (erro do bind, pode já haver um app utilizando a porta)
+                    - IP fornecido para determinar a interface utilizada (?) 
+                5) Primitiva Listen
+                    - Se o sistema for rodado com apenas o bind executado, o sistema op irá negar o acesso
+                    - Aplicativo pronto pra receber as conexões 
+                6) Accept**
+                    - Comando bloqueante
+                        - Bloqueia o fluxo da execução do programa
+                        - cin do C++, input, etc
+                            - Só continua a execução após a reação do cliente
+                    - Cliente da o connect para conseguir fazer a conexão naquela aplicação
+                        - Servidor recebe IP e a porta utilizada
+                7) Túnel de conexão criado
 
-          
+                8) Após 7), Cliente há troca de informações com o servidor 
+                    - Write
+                        - escrita através de bits
+                    - Protocolo - forma em que se organizam os bits
+
+
+                *) Exceções: erros não previstos
+                    - Programa executando e a energia cai
+                    - Geradas a nivel de sistema op de modo a criar um modo alternativo de tratar essas circuinstâncias
+                      a fim de não criar grandes inconsistências no algoritmo
+                    - Criação de blocos passíveis de exceção
+                        - se ela ocorrer, criar uma exceção, except, para criar o caminho alternativo
+                
+*****************************************************************************************************************************8
+
+procurar no google "socket python"
+
+
+                
+
 */
