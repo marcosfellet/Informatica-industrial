b382d8b8cc2831da48dd133be3d4988a2a0cbf62
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-11-06 15:15:57 -0300

    Create Notas.cpp

---
+++ Aula14/Notas.cpp
@@ -0,0 +1,13 @@
+/*
+MAC => protocolo de rede local
+IP => comunicação lógica
+Camada de rede => conexão
+Camada de transporte => aplicação
+
+API Socket => recurso do sis op para que os programadores possam acessar a interface de rede (ethernet e wifi) 
+
+
+
+
+
+*/
