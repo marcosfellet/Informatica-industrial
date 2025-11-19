b13532fcc530ddb747e59980aeb5f22152dbfebd
marcosfellet <130519485+marcosfellet@users.noreply.github.com>
2025-11-19 11:48:04 -0300

    Create Denis.py

---
+++ Denis.py
@@ -0,0 +1,28 @@
+#################################################################################################
+                        Documentação do progresso do grupo de Python
+#################################################################################################
+
+Afazeres:
+    1 - Fazer monitorar o ESP 
+        i - Ler valores de corrente, tensão, temperatura, 
+
+    2 - Comunicação via Bluetooth
+
+    3 - Organizar o código 
+        i - Torná-lo legível e fácil de se utilizar/modificar
+        ii - Na leitura, há valores sendo mandados, sendo um processo desnecessário  
+
+    4 - Testar mudanças
+        i - Confirmar se o algoritmo funciona em computadores que nunca rodaram o código 
+
+Sendo feito:
+    1 - Documentação 
+
+    2 - 
+
+Concluído:
+    1 - Protocolo de comunicação
+
+
+Observações:
+    1 - Bibliotecas por conta do grupo do ESP
